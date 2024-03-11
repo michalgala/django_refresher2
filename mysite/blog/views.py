713bb9be7fa5f90b18a6c612652d6310b90ee1bd
Michal Gala <you@example.com>
2024-03-11 20:13:52 +0100

    firt

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.views.generic import ListView
 
 # Create your views here.
 def post_list(request):
@@ -19,3 +19,12 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
